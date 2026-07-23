7084fadccaef03094afd61ff89d55e62eeac5b85
Lucass-miranda <lucas777.paredes11@gmail.com>
2026-07-23 20:03:09 -0300

    Add initial Python script with comments and variables

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Lucas-Miranda.py
@@ -0,0 +1,17 @@
+#mi primer comentario https://www.python.org/
+
+''' estos son los
+comentarios
+largos'''
+
+mi.variable = ' esta es mi variable '
+
+mi.variable.int = ' 2 '
+
+mi,variable.float = ' 1.4 '
+
+mi.variable.bool = ' true '
+
+mi.variable.bool = ' false '
+
+print('hola, python')  
